b314f0e99b4b60d119333cbdf7cbcda25b1b3354
Fernanda Diniz <maria.diniz@dcomp.ufs.br>
2025-12-13 21:48:21 -0300

    style: ajusta hero e tema com alinhamento e botões arredondados

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,46 @@
 import Home from './pages/Home/Home'
 import { Global, css } from '@emotion/react'
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+
+const theme = createTheme({
+
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#971111ff',
+      contrastText: '#fff',
+    },
+    secondary: {
+      main: '#b1b1b1ff',
+    },
+    background: {
+      default: '#000000',
+      paper: '#111111',
+    },
+    text: {
+      primary: '#ffffff',
+      secondary: '#a1a1a1ff',
+    },
+  },
+
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          borderRadius: 150, // pílula
+          textTransform: 'none',
+          paddingInline: 24,
+          paddingBlock: 12,
+        },
+      },
+    },
+  },
+})
+
 
 function App() {
-
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Global styles={css`
         html, body, #root {
           width: 100%;
@@ -14,7 +50,7 @@ function App() {
         }
       `} />
       <Home />
-    </>
+    </ThemeProvider>
   )
 }
 
